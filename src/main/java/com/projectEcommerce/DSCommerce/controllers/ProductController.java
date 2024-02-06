948e31f728c9bc4dbd95c78525f03476902078f1
Andressa Ribeiro <andressa.ribeiro@tatodesk.com>
2024-02-06 10:00:46 -0300

    feat: get products paged

--- src/main/java/com/projectEcommerce/DSCommerce/controllers/ProductController.java
+++ src/main/java/com/projectEcommerce/DSCommerce/controllers/ProductController.java
@@ -5,11 +5,14 @@ import com.projectEcommerce.DSCommerce.entities.Product;
 import com.projectEcommerce.DSCommerce.repositories.ProductRepository;
 import com.projectEcommerce.DSCommerce.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
 import java.util.Optional;
 
 @RestController
@@ -22,5 +25,10 @@ public class ProductController {
     @GetMapping(value = "/{id}")
     public ProductDTO findById(@PathVariable Long id) {
         return service.findById(id);
+    }
+
+    @GetMapping
+    public Page<ProductDTO> finAll(Pageable pageable) {
+        return service.findAll(pageable);
     }
 }
