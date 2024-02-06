08c25fb5cf3cdb0a16407276e2b477898e17fb6e
Andressa Ribeiro <andressa.ribeiro@tatodesk.com>
2024-02-06 10:26:31 -0300

    feat: post(insert) new products

--- src/main/java/com/projectEcommerce/DSCommerce/services/ProductService.java
+++ src/main/java/com/projectEcommerce/DSCommerce/services/ProductService.java
@@ -9,6 +9,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.sql.PreparedStatement;
 import java.util.List;
 import java.util.Optional;
 
@@ -28,6 +29,18 @@ public class ProductService {
     public Page<ProductDTO> findAll(Pageable pageable) {
         Page<Product> result = repository.findAll(pageable);
         return result.map(x -> new ProductDTO(x));
+    }
 
+    @Transactional
+    public ProductDTO insert(ProductDTO dto) {
+        Product entity = new Product();
+        entity.setName(dto.getName());
+        entity.setDescription(dto.getDescription());
+        entity.setPrice(dto.getPrice());
+        entity.setImgUrl(dto.getImgUrl());
+
+        entity = repository.save(entity);
+
+        return new ProductDTO(entity);
     }
 }
